cd9956d8f73a8f8f529040a36cb64f662d7cf787
Ricardo Meza Sanchez <ing.ricardomeza@gmail.com>
2016-04-12 23:58:15 -0500

    Initializing the User when the App loads, logout

--- public/scripts/userController.js
+++ public/scripts/userController.js
@@ -6,7 +6,7 @@
 		.module('authApp')
 		.controller('UserController', UserController);
 
-	function UserController($http) {
+	function UserController($http, $auth, $rootScope, $state) {
 
 		var vm = this;
 
@@ -15,12 +15,33 @@
 
 		vm.getUsers = function() {
 
-			// This request will hit the index method in the AuthenticateController
-			// on the Laravel side and will return the list of users
+			//Grab the list of users from the API
 			$http.get('api/authenticate').success(function(users) {
 				vm.users = users;
 			}).error(function(error) {
 				vm.error = error;
+			});
+		}
+
+		// We would normally put the logout method in the same
+		// spot as the login method, ideally extracted out into
+		// a service. For this simpler example we'll leave it here
+		vm.logout = function() {
+
+			$auth.logout().then(function() {
+
+				// Remove the authenticated user from local storage
+				localStorage.removeItem('user');
+
+				// Flip authenticated to false so that we no longer
+				// show UI elements dependant on the user being logged in
+				$rootScope.authenticated = false;
+
+				// Remove the current user info from rootscope
+				$rootScope.currentUser = null;
+
+				// Redirect to auth (necessary for Satellizer 0.12.5+)
+				$state.go('auth');
 			});
 		}
 	}
