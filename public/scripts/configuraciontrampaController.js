edf0ca6b7fcf52c2125909d0049597a85e7c6eb4
Ricardo Meza Sanchez <ing.ricardomeza@gmail.com>
2016-04-26 23:45:42 -0500

    populate select tipo trampa from json variable

--- public/scripts/configuraciontrampaController.js
+++ public/scripts/configuraciontrampaController.js
@@ -12,22 +12,31 @@
 
 		vm.trampas;
 		vm.error;
+		vm.tipotrampas;
+		vm.tipotrampas=[
+			{countryId : 1, name : "France - Mainland", desc: "some description" },
+			{countryId : 2, name : "Gibraltar", desc: "some description"},
+			{countryId : 3, name : "Malta", desc: "some description"}
+			];
+			vm.selectedTrampa = angular.copy(vm.tipotrampas[0]);
+
 
 		//vm.getTrampas = function() {
 
-			//Grab the list of trampas from the API
-			$http.get('api/configuraciontrampa').success(function(trampas) {
-				vm.trampas = trampas;
-			}).error(function(error) {
-				vm.error = error;
-			});
-	//	}
+		//Grab the list of trampas from the API
+		$http.get('api/configuraciontrampa').success(function(trampas) {
+			vm.trampas = trampas;
+		}).error(function(error) {
+			vm.error = error;
+		});
+		//	}
 
-	  vm.addTrampa = function() {
-    $scope.movie.$save(function() {
-      $state.go('trampas'); 
-    });
-  };
+		vm.addTrampa = function() {
+			$scope.movie.$save(function() {
+				$state.go('trampas');
+			});
+		};
+
 
 	}
 
