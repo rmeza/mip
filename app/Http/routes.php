f1721d3503949a8ac48d3f0fbd33202abfc41ec0
perlamarina <perlamarina.franco@gmail.com>
2016-05-16 10:05:43 -0500

    Storing configuration of trampa

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,13 +14,15 @@ Route::get('/', function () {
 });
 Route::group(['prefix' => 'api'], function()
 {
-	Route::resource('authenticate', 'AuthenticateController', ['only' => ['index']]);
-	Route::post('authenticate', 'AuthenticateController@authenticate');
-	Route::get('authenticate/user','AuthenticateController@getAuthenticatedUser');
-  Route::get('configuraciontrampa','ConfiguraciontrampaController@index');
-  Route::post('configuraciontrampa','ConfiguraciontrampaController@store');
-  Route::get('tipotrampa','TipotrampaController@index');
-  Route::get('clasificaciontrampa','ClasificaciontrampaController@index');
-  Route::get('ubicaciontrampa','UbicaciontrampaController@index');
-  Route::get('planta','PlantaController@index');
+
+    Route::resource('authenticate', 'AuthenticateController', ['only' => ['index']]);
+    Route::post('authenticate', 'AuthenticateController@authenticate');
+    Route::get('authenticate/user','AuthenticateController@getAuthenticatedUser');
+    Route::get('configuraciontrampa','ConfiguraciontrampaController@index');
+    Route::post('configuraciontrampa','ConfiguraciontrampaController@store');
+    Route::get('tipotrampa','TipotrampaController@index');
+    Route::get('clasificaciontrampa','ClasificaciontrampaController@index');
+    Route::get('ubicaciontrampa','UbicaciontrampaController@index');
+    Route::get('planta','PlantaController@index');
+
 });
