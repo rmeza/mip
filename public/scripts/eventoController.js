41b705317bbd5421f5cd54eb5b4fadbee53c8d94
perlamarina <perlamarina.franco@gmail.com>
2016-06-07 13:24:30 -0500

    fix for the detailed view of the eventos

--- public/scripts/eventoController.js
+++ public/scripts/eventoController.js
@@ -6,7 +6,7 @@
 	.module('authApp')
 	.controller('EventoController',EventoController,['$u'])
 	
-	function EventoController($http, $auth, $rootScope,$state,$scope,$uibModal, $log) {
+	function EventoController($http, $auth, $rootScope,$state,$scope) {
 
 
 		var vm = this;
@@ -23,14 +23,17 @@
 		.error(function(error) {
 			vm.error = error;
 		});
-
-		//Grab the list of eventos from the API
-		
-
 		
 
 		//todo probar que funcione
 		vm.showDetalle=function (id){
+			console.log("valor del activo:"+vm.active)
+			if (vm.active != id) {
+			vm.active = id;
+			}
+			else {
+			vm.active = null;
+			}
 
 			$http.get('api/showdetalleevento/'+id)
 			.success(function(detalles) {
