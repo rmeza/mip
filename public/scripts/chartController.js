1b197729e7087dda98c96cc476e250cbf50f80fe
Ricardo Meza <rsanchez@pdxinc.com>
2016-06-08 17:00:48 -0500

    first chart values static

--- public/scripts/chartController.js
+++ public/scripts/chartController.js
@@ -10,6 +10,42 @@
 
 		var vm = this;
 
+		var chart = c3.generate({
+			bindto: '#chart',
+			data: {
+
+				json:[
+					{name:'4/1/2016',palomilla:30,mosquito:50},
+					{name:'11/1/2016',palomilla:50,mosquito:60},
+					{name:'18/1/2016',palomilla:60,mosquito:90},
+					{name:'25/1/2016',palomilla:80,mosquito:40},
+					{name:'8/2/2016',palomilla:70,mosquito:100},
+
+				],
+				keys: {
+					value:['palomilla','mosquito']
+				},
+				type: 'bar',
+
+			},
+			grid:{
+				y:{
+					show:true
+				}
+			},
+			axis:{
+				x: {
+					label: 'Semanas',
+					type:'category',
+					categories:['4/1/2016','11/1/2016','18/1/2016','25/1/2016','1/2/2016','8/2/2016']
+				},
+				y:{
+					label:'Cantidad'
+				},
+
+			}
+
+		});
 
 	}
 
